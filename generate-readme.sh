88b508a29a44ecc854d8c4883c3a50e5e462d8f4
victorreiscarlota <victor.reis.carlotta@gmail.com>
2024-09-09 23:35:21 -0300

    removido comentários desnecessários

--- generate-readme.sh
+++ generate-readme.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-# Caminho para o arquivo de configuração com datas e resenhas
+
 CONFIG_FILE="./config.txt"
 
-# Caminho para a pasta de resenhas
+
 CAMINHO_RESENHAS="./resenhas/"
 
-# Caminho para o arquivo README.md
+
 CAMINHO_README="./README.md"
 
-# Obtém a data atual no formato YYYY-MM-DD
+
 DATA_ATUAL=$(date +"%Y-%m-%d")
 
-# Inicializa variáveis
+
 RESENHA_ATUAL=""
 NOME_ARTIGO=""
 
-# Lê o arquivo de configuração para encontrar a resenha correspondente à data atual
+
 while IFS=',' read -r data_entrega nome_arquivo nome_artigo; do
     if [[ "$DATA_ATUAL" == "$data_entrega" || "$DATA_ATUAL" > "$data_entrega" ]]; then
         RESENHA_ATUAL=$nome_arquivo
@@ -24,13 +24,13 @@ while IFS=',' read -r data_entrega nome_arquivo nome_artigo; do
     fi
 done < "$CONFIG_FILE"
 
-# Se nenhuma resenha foi encontrada, sai com erro
+
 if [ -z "$RESENHA_ATUAL" ]; then
     echo "Nenhuma resenha encontrada para a data atual."
     exit 1
 fi
 
-# Cabeçalho do README.md
+
 HEAD="\
 # Resenhas de Artigos
 
@@ -53,6 +53,6 @@ Caso opte, pode acessar a resenha de um artigo específico clicando no link abai
 ## Resenha
 "
 
-# Gerar o conteúdo completo do README.md
+
 echo "$HEAD" > $CAMINHO_README
 cat "$CAMINHO_RESENHAS$RESENHA_ATUAL" >> $CAMINHO_README
